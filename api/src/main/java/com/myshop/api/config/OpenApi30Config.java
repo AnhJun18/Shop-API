c81e52aa36ab72ab543ee1e1d5ac09d965ff9f3d
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-09-18 11:41:39 +0700

    update swagger 2

--- api/src/main/java/com/myshop/api/config/OpenApi30Config.java
+++ api/src/main/java/com/myshop/api/config/OpenApi30Config.java
@@ -1,2 +1,20 @@
-package com.myshop.api.config;public class OpenApi30Config {
+package com.myshop.api.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Shop API", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApi30Config {
+
 }
