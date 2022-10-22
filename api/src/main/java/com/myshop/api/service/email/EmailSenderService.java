9946777944ce8c28bc5afa11eda77a9b80a06d0f
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-10-22 07:35:55 +0700

    send email with thymeleaf 2

--- api/src/main/java/com/myshop/api/service/email/EmailSenderService.java
+++ api/src/main/java/com/myshop/api/service/email/EmailSenderService.java
@@ -1,14 +1,21 @@
 package com.myshop.api.service.email;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
 
 @Service
 public class EmailSenderService {
@@ -16,24 +23,29 @@ public class EmailSenderService {
     private JavaMailSender mailSender;
     private SpringTemplateEngine springTemplateEngine;
 
-    public void sendEmail() {
+    public void sendEmail()  {
         String from = "anhle180101@gmail.com";
         String to = "n19dccn006@student.ptithcm.edu.vn";
 
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
 
-
         try {
             helper.setFrom(from);
             helper.setTo(to);
             helper.setSubject("This is an HTML email");
-            Context ctx = new Context();
-            String  htmlContent = springTemplateEngine.process("register",ctx);
 
-            helper.setText(htmlContent, true);
+            ResourceLoader loader = new DefaultResourceLoader();
+            MustacheFactory mf = new DefaultMustacheFactory(loader.getResource("classpath:/templates/").getFile());
+            Mustache mustache = mf.compile( "register.html");
+            StringWriter htmlContent = new StringWriter();
+            mustache.execute(htmlContent, Map.of("link", "https://abc.com","name","Anh Jun"));
+
+            helper.setText(htmlContent.toString(), true);
             mailSender.send(message);
         } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
