0436a7b8c470caa75a33847b22840cb19033407f
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-11-24 22:32:54 +0700

    add warehouse receipt

---
+++ repository/src/main/java/com/myshop/repositories/warehouse/entity/WarehouseReceiptDetail.java
@@ -0,0 +1,2 @@
+package com.myshop.repositories.warehouse.entity;public class WarehouseReceiptDetail {
+}
---
+++ repository/src/main/java/com/myshop/repositories/warehouse/repos/WarehouseReceiptDetailRepository.java
@@ -0,0 +1,2 @@
+package com.myshop.repositories.warehouse.repos;public interface WarehouseReceiptDetailRepository {
+}
