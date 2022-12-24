7f2362d8845a0949bed4ef6c5e8ac2626a520306
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-12-24 11:58:00 +0700

    block create acc admin from user

--- api/src/main/java/com/myshop/api/service/user/UserServiceImpl.java
+++ api/src/main/java/com/myshop/api/service/user/UserServiceImpl.java
@@ -131,6 +131,9 @@ public class UserServiceImpl extends CRUDBaseServiceImpl<UserInfo, UserRequest, 
         if (role == null) {
             return UserResponse.builder().status(false).message("Role is not exists, please check again").build();
         }
+        if (role.getName() == "ROLE_ADMIN") {
+            return UserResponse.builder().status(false).message("You do not have permission to create an admin account").build();
+        }
         account = Account.builder().username(userRequest.getUserName()).deleteFlag(false).role(role).email(userRequest.getEmail())
                 .password(passwordEncoder.encode(userRequest.getPassword() + Constants.SALT_DEFAULT)).build();
         UserInfo userInfo = UserInfo.builder().firstName(userRequest.getFirstName())
