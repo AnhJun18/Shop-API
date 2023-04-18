6ad111467ec14c0ed1cb2cf091b47a7fcea57a6f
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2023-04-18 15:57:49 +0700

    add GHTK config

---
+++ api/src/main/java/com/myshop/api/config/GHTKConfig.java
@@ -0,0 +1,15 @@
+package com.myshop.api.config;
+
+public class GHTKConfig {
+
+    public final static String TOKEN = "700a2a511c4730b289ad27ab100b9de838ca07bd";
+    public final static String API_ENDPOINT = "https://services.giaohangtietkiem.vn";
+    public final static String CREATE_ORDER_URL = API_ENDPOINT + "/services/shipment/order/?ver=1.5";
+    public final static String CALCUATE_SHIP_FEE_URL = API_ENDPOINT + "/services/shipment/fee";
+    public final static String CHECKING_STATUS_URL = API_ENDPOINT + "/services/shipment/v2/";
+    public final static String PRINT_INVOICE = API_ENDPOINT + "/services/label/";
+    public final static String CANCEL_ORDER_URL = API_ENDPOINT + "/services/shipment/cancel/";
+    public final static String PICK_PROVINCE ="TP Hồ Chí Minh";
+    public final static String PICK_DISTRICT ="Tăng Nhơn Phú A";
+
+}
