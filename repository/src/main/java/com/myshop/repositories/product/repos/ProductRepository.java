20e80fc697a34971b2fc989714528d0d813d82cd
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2023-12-12 22:15:57 +0700

    update

--- repository/src/main/java/com/myshop/repositories/product/repos/ProductRepository.java
+++ repository/src/main/java/com/myshop/repositories/product/repos/ProductRepository.java
@@ -2,10 +2,18 @@ package com.myshop.repositories.product.repos;
 
 import com.myshop.repositories.product.entities.Product;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long>, JpaSpecificationExecutor<Product> {
-
+  @Modifying
+  @Transactional
+  @Query(value = "INSERT INTO [dbo].[PRICEVOLATILITY]([PRODUCTID],[EMPLOYEEID],[TIMECHANGE],[PRICE]) " +
+          "     VALUES(:productId,:employeeId,:timeChange,:newPrice)", nativeQuery = true)
+  void updatePrice(@Param("productId") Long productId, @Param("employeeId") Long employeeId, @Param("timeChange") String timeChange,@Param("newPrice") Long newPrice);
 }
