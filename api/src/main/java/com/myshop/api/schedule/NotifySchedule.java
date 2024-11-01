9806d633b0a360a721e5893ecfd1ffbfc1ccf490
Le Phuong Anh <81857289+AnhJun18@users.noreply.github.com>
2024-11-01 19:25:18 +0000

    update

---
+++ api/src/main/java/com/myshop/api/schedule/NotifySchedule.java
@@ -0,0 +1,19 @@
+package com.myshop.api.schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.myshop.api.service.notify.INotifyService;
+
+@Component
+public class NotifySchedule {
+    @Autowired
+    INotifyService notifyService;
+    private final static String TK_GR_QC = "Y5OKJTawfVbMoOfMfKtFbXO6wq3QJo6qE1ocde1YvKE";
+
+    @Scheduled(cron = "0 0,30 7,8,16,17 * * 1-7")
+    public void scheduleTaskWithFixedRate() {
+        notifyService.sendNotify(TK_GR_QC, "Chấm công đê bà con");
+    }
+}
